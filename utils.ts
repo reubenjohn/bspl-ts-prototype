31664fd06c7eab4285e1d2a1d19549c392d40ccd
Reuben John <reubenvjohn@gmail.com>
2022-05-04 08:49:34 -0400

    Remove unused util functions

--- utils.ts
+++ utils.ts
@@ -1,19 +1,3 @@
-type FilteredProperties<SRC extends object, KEYS extends (keyof SRC)[]> = { [K in KEYS[number]]: SRC[K] };
-
-export function filterProperties<SRC extends object>(source: SRC, keysToKeep: (keyof SRC)[]): SRC {
-    const result: Partial<SRC> = {};
-    for (let key of keysToKeep)
-        result[key] = source[key];
-    return <FilteredProperties<SRC, typeof keysToKeep>>result;
-}
-
-export function containsProperties(obj: object, keys: string[]): boolean {
-    for (let paramName of keys)
-        if (!(paramName in obj))
-            return false;
-    return true;
-}
-
 export function minus<A extends object, B extends object>(from: A, toRemove: B): Exclude<A, B> {
     const result: { [key: string]: any } = {};
     for (let key in from)
